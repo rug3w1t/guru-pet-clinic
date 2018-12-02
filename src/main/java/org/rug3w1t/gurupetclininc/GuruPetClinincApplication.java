ac4ef9dabbdb3087621dcb5f71c7d113db62eca8
rug3w1t <rug3w1t@gmail.com>
2018-12-02 11:08:38 +0100

    initial comit

---
+++ src/main/java/org/rug3w1t/gurupetclininc/GuruPetClinincApplication.java
@@ -0,0 +1,12 @@
+package org.rug3w1t.gurupetclininc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruPetClinincApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuruPetClinincApplication.class, args);
+	}
+}
